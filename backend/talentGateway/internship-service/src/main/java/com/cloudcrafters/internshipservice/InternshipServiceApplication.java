ab63deeb796d38d28940340d8be6b6877fbc9c69
OnsFadhele <fadhel.ons@esprit.tn>
2024-03-20 15:50:09 +0100

    first push spring cloud crafters

---
+++ backend/talentGateway/internship-service/src/main/java/com/cloudcrafters/internshipservice/InternshipServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloudcrafters.internshipservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipServiceApplication.class, args);
+    }
+
+}
---
+++ backend/talentGateway/internship-service/src/test/java/com/cloudcrafters/internshipservice/InternshipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudcrafters.internshipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
