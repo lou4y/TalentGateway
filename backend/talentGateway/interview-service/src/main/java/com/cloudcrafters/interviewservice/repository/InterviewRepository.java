1cfa45c861e2e105964c06cd89b4499469806d14
aahmed111 <ahmed.hmila@esprit.tn>
2024-03-22 17:48:17 +0100

    resolve conflit

--- backend/talentGateway/interview-service/src/main/java/com/cloudcrafters/interviewservice/repository/InterviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cloudcrafters.interviewservice.repository;
-import com.cloudcrafters.interviewservice.model.Interview;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InterviewRepository extends MongoRepository<Interview, String> {
-
-
-
-}
