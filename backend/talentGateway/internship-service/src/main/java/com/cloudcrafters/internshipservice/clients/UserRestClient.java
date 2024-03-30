95f624bec4a8477fbb142db6e2f875d0b774d7f0
marwen69 <marouene.hammami@esprit.tn>
2024-03-30 17:08:27 +0100

    update work add scheduled task

--- backend/talentGateway/internship-service/src/main/java/com/cloudcrafters/internshipservice/clients/UserRestClient.java
+++ backend/talentGateway/internship-service/src/main/java/com/cloudcrafters/internshipservice/clients/UserRestClient.java
@@ -2,6 +2,8 @@ package com.cloudcrafters.internshipservice.clients;
 
 import com.cloudcrafters.internshipservice.entites.Category;
 import com.cloudcrafters.internshipservice.entites.Internship;
+import com.cloudcrafters.internshipservice.enums.SkillLevel;
+import com.cloudcrafters.internshipservice.models.Skill;
 import com.cloudcrafters.internshipservice.models.User;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -19,8 +21,7 @@ public interface UserRestClient {
 
     @GetMapping("/users")
     List<User> getAllUsers();
-
-
+    // Methods for User entity
 
     default User getDefaultUser(String id, Exception exception) {
         User user = new User();
@@ -33,5 +34,23 @@ public interface UserRestClient {
 
     default List<User> getDefaultAllUsers(Exception exception) {
         return List.of();
+    }
+
+
+    // Methods for Skill entity
+    @GetMapping("/profile/skill/{id}")
+    Skill findSkillById(@PathVariable Long id);
+
+    @GetMapping("/profile/skills/{userId}")
+    List<Skill> findSkillsByUserId(@PathVariable String userId);
+
+
+    // Default method for handling fallback for skill operations
+    default Skill getDefaultSkill(Long id, Exception exception) {
+        Skill skill = new Skill();
+        skill.setId(id);
+        skill.setSkillName("Not available");
+        skill.setSkillLevel(SkillLevel.BEGINNER); // Set default skill level
+        return skill;
     }
 }
