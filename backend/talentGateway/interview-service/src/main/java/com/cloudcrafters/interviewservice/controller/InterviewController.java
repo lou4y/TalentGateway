c941a9dc4b745fcde5890b78ef701ebcef0e1321
ahmed-hmila <ahmed.hmila@esprit.tn>
2024-04-09 14:02:28 +0100

    fix a probleme

--- backend/talentGateway/interview-service/src/main/java/com/cloudcrafters/interviewservice/controller/InterviewController.java
+++ backend/talentGateway/interview-service/src/main/java/com/cloudcrafters/interviewservice/controller/InterviewController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/interview")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class InterviewController {
 
     private final InterviewService interviewService;
