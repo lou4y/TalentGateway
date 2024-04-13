41b466249c05908e8decee72f048ae4675ca1fe4
lou4y <louay.ghazouani@esprit.tn>
2024-04-13 20:37:44 +0100

    all user skills

--- backend/talentGateway/user-service/src/main/java/com/cloudcrafters/userservice/keycloakclient/UserResource.java
+++ backend/talentGateway/user-service/src/main/java/com/cloudcrafters/userservice/keycloakclient/UserResource.java
@@ -3,6 +3,7 @@ package com.cloudcrafters.userservice.keycloakclient;
 import com.cloudcrafters.userservice.dto.Role;
 import com.cloudcrafters.userservice.dto.User;
 import com.cloudcrafters.userservice.security.KeycloakSecurityUtil;
+import com.cloudcrafters.userservice.services.SkillService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.ws.rs.core.Response;
 import org.keycloak.admin.client.Keycloak;
@@ -25,6 +26,8 @@ public class UserResource {
 	
 	@Autowired
 	KeycloakSecurityUtil keycloakUtil;
+	@Autowired
+	SkillService skillService;
 	
 	@Value("${realm}")
 	private String realm;
@@ -37,6 +40,18 @@ public class UserResource {
 				keycloak.realm(realm).users().list();
 		return mapUsers(userRepresentations);
     }
+	@GetMapping
+	@RequestMapping("/usersskills")
+	public List<User> getUsersSkills() {
+		Keycloak keycloak = keycloakUtil.getKeycloakInstance();
+		List<UserRepresentation> userRepresentations =
+				keycloak.realm(realm).users().list();
+		List <User> users = mapUsers(userRepresentations);
+		for (User user: users) {
+			user.setSkills(skillService.getAllSkillsByUser(user.getId()));
+		}
+		return users;
+	}
 	
 	@GetMapping(value = "/users/{id}")
 	public User getUser(@PathVariable("id") String id) {
--- frontend/talentgatewayFrontEnd/src/app/app-routing.module.ts
+++ frontend/talentgatewayFrontEnd/src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { Page404Component } from './extrapages/page404/page404.component';
 
 const routes: Routes = [
   // tslint:disable-next-line: max-line-length
-  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] },
-  { path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule), canActivate: [AuthGuard] },
+  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)/*, canActivate: [AuthGuard]*/ },
+  { path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule)/*, canActivate: [AuthGuard]*/ },
   { path: 'crypto-ico-landing', component: CyptolandingComponent },
   { path: '**', component: Page404Component },
 ];
