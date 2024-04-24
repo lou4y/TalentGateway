7d8fd9548d3a4c6d38ff23f1b311a1db5ae3bf63
med-las <lassoued.mohamed@esprit.tn>
2024-04-24 10:40:43 +0100

    Integration ZegoCloud

--- frontend/talentgatewayFrontEnd/src/app/chatComponents/video-conference/video-conference.component.ts
+++ frontend/talentgatewayFrontEnd/src/app/chatComponents/video-conference/video-conference.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../../core/services/auth.service";
 
-
-declare const ZegoUIKitPrebuilt: any;
+declare var ZegoUIKitPrebuilt: any;
 
 @Component({
   selector: 'app-video-conference',
@@ -10,56 +10,56 @@ declare const ZegoUIKitPrebuilt: any;
 })
 export class VideoConferenceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { } // Inject the AuthenticationService
 
   ngOnInit(): void {
-    window.onload = () => {
-      this.initializeVideoConference();
-    };
+    this.setupVideoConference();
   }
 
+  setupVideoConference(): void {
+    const roomID = this.getUrlParams(window.location.href)['roomID'] || (Math.floor(Math.random() * 10000) + "");
 
-  initializeVideoConference(): void {
-    function getUrlParams(url: string) {
-      let urlStr = url.split('?')[1];
-      const urlSearchParams = new URLSearchParams(urlStr);
-      const result: any = {};
-      urlSearchParams.forEach((value, key) => {
-        result[key] = value;
+    // Fetch current user ID and username from AuthenticationService
+    this.authService.currentUser().then(user => {
+      const userID = user.username;
+      const userName = user.username;
+
+      const appID = 939715686;
+      const serverSecret = "0b03a70933affa672bc6ec71d4de390e";
+      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, userID, userName);
+
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      zp.joinRoom({
+        container: document.querySelector("#root"),
+        sharedLinks: [{
+          name: 'Personal link',
+          url: window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID,
+        }],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.VideoConference,
+        },
+
+        turnOnMicrophoneWhenJoining: false,
+        turnOnCameraWhenJoining: false,
+        showMyCameraToggleButton: true,
+        showMyMicrophoneToggleButton: true,
+        showAudioVideoSettingsButton: true,
+        showScreenSharingButton: true,
+        showTextChat: true,
+        showUserList: true,
+        maxUsers: 2,
+        layout: "Auto",
+        showLayoutButton: false,
       });
-
-      return result;
-    }
-
-    const roomID = getUrlParams(window.location.href)['roomID'] || (Math.floor(Math.random() * 10000) + "");
-    const userID = Math.floor(Math.random() * 10000) + "";
-    const userName = "userName" + userID;
-    const appID = 1424078894;
-    const serverSecret = "ab640f0aa60b6e99bf852bc645462216";
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, userID, userName);
-
-    const zp = ZegoUIKitPrebuilt.create(kitToken);
-    zp.joinRoom({
-      container: document.querySelector("#root"),
-      sharedLinks: [{
-        name: 'Personal link',
-        url: window.location.protocol + '//' + window.location.host  + window.location.pathname + '?roomID=' + roomID,
-      }],
-      scenario: {
-        mode: ZegoUIKitPrebuilt.VideoConference,
-      },
-      turnOnMicrophoneWhenJoining: false,
-      turnOnCameraWhenJoining: false,
-      showMyCameraToggleButton: true,
-      showMyMicrophoneToggleButton: true,
-      showAudioVideoSettingsButton: true,
-      showScreenSharingButton: true,
-      showTextChat: true,
-      showUserList: true,
-      maxUsers: 2,
-      layout: "Auto",
-      showLayoutButton: false,
+    }).catch(error => {
+      console.error('Error fetching current user:', error);
     });
   }
-}
 
+  getUrlParams(url: string): any {
+    let urlStr = url.split('?')[1];
+    const urlSearchParams = new URLSearchParams(urlStr);
+    const result = Object.fromEntries(urlSearchParams.entries());
+    return result;
+  }
+}
